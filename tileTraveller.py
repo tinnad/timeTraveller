488087b9a4d9918e4577bbaba61bc7118bb0f519
Tinna Dofradottir <Tinnadofradottir@Tinnas-MacBook-Pro.local>
2018-09-21 19:11:58 +0000

    tileTraveller ready to go

--- tileTraveller.py
+++ tileTraveller.py
@@ -1,92 +1,88 @@
 location = 11
-invalidinput = False
+invalidInput = False
 while location != 31:
-
-    if location == 11 or location == 21:
-        if invalidinput == False:
+    if location == 11 or location == 21 or location == 31:
+        
+        if invalidInput == False:
             print('You can travel: (N)orth.')
-        direction = input('Direction: ')
-        if direction == 'n' or direction == 'N':
+        dire = input('Direction: ')
+        if dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-    if location == 12:
-        if invalidinput == False:
+    elif location == 12:
+        
+        if invalidInput == False:
             print('You can travel: (N)orth or (E)ast or (S)outh.')
-        direction = input('Direction: ')
-        if direction == 's' or direction == 'S':
-            location -= 1
-        elif direction == 'e' or direction == 'E':
-            location += 10
-        elif direction == 'n' or direction == 'N':
+        dire = input('Direction: ')
+        if dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
+        elif dire == 'e' or dire == 'E':
+            invalidInput = False
+            location += 10
+        elif dire == 's' or dire == 'S':
+            invalidInput = False
+            location -= 1
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-    if location == 13:
-        if invalidinput == False:
+    elif location == 13:
+        
+        if invalidInput == False:
             print('You can travel: (E)ast or (S)outh.')
-        direction = input('Direction: ')
-        if direction == 's' or direction == 'S':
-            location -= 1
-        elif direction == 'e' or direction == 'E':
+        dire = input('Direction: ')
+        if dire == 'e' or dire == 'E':
+            invalidInput = False
             location += 10
+        elif dire == 's' or dire == 'S':
+            invalidInput = False
+            location -= 1
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-    if location == 23:
-        if invalidinput == False:
+    elif location == 23:
+        
+        if invalidInput == False:
             print('You can travel: (E)ast or (W)est.')
-        direction = input('Direction: ')
-        if direction == 'e' or direction == 'E':
+        dire = input('Direction: ')
+        if dire == 'e' or dire == 'E':
+            invalidInput = False
             location += 10
-        elif direction == 'w' or direction == 'W':
+        elif dire == 'w' or dire == 'W':
+            invalidInput = False
             location -= 10
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-    if location == 33 or location == 22: 
-        if invalidinput == False:
-            print('You can travel: (W)est or (S)outh.')
-        direction = input('Direction: ')
-        if direction == 'w' or direction == 'W':
-            location -= 10
-        elif direction == 's' or direction == 'S':
+    elif location == 22 or location == 33:
+        
+        if invalidInput == False:
+            print('You can travel: (S)outh or (W)est.')
+        dire = input('Direction: ')
+        if dire == 's' or dire == 'S':
+            invalidInput = False
             location -= 1
+        elif dire == 'w' or dire == 'W':
+            invalidInput = False
+            location -= 10
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-    if location == 32:
-        if invalidinput == False:
+    else: # location = 32
+        
+        if invalidInput == False:
             print('You can travel: (N)orth or (S)outh.')
-        direction = input('Direction: ')
-        if direction == 'N' or direction == 'n':
+        dire = input('Direction: ')
+        if dire == 's' or dire == 'S':
+            invalidInput = False
+            location -= 1
+        elif dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
-        elif direction == 's' or direction == 'S':
-           location -= 1
         else:
-            invalidinput = True
+            invalidInput = True
             print('Not a valid direction!')
-            direction = input('Direction: ')
-
-print('Victory')
-
-
-    
-  
-
-
-
-
-                
+print('Victory!')
--- tileTravellerdef.py
+++ tileTravellerdef.py
@@ -1,33 +1,120 @@
-def move():
-    if direction == 'n' or direction == 'N':
-        location += 1
-    elif direction == 's' or direction == 'S':
-        location -= 1
-    elif direction == 'e' or direction == 'E':
-        location += 10
-    elif direction == 'w' or direction == 'W':
-        location -= 10
-    return location
+def tile11(direction):
+    while direction != 'N':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'N':
+            return 1.2
+        else:
+            print('Not a valid direction!')
 
-def get_direction():
-    if location == 11 or location == 21:
+def tile12(direction):
+    while direction != 'N' and direction != 'S' and direction != 'E':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'N':
+            return 1.3
+        elif direction == 'E':
+            return 2.2
+        elif direction == 'S':
+            return 1.1
+        else:
+            print('Not a valid direction!')
+def tile13(direction):
+    while direction != 'E' and direction != 'S':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'E':
+            return 2.3
+        elif direction == 'S':
+            return 1.2
+        else:
+            print('Not a valid direction!')
+
+def tile21(direction):
+    while direction != 'N':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'N':
+            return 2.2
+        else:
+            print('Not a valid direction!')
+
+def tile22(direction):
+    while direction != 'W' and direction != 'S':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'S':
+            return 2.1
+        elif direction == 'W':
+            return 1.2
+        else:
+            print('Not a valid direction!')
+def tile23(direction):
+            while direction != 'E' and direction != 'W':
+                direction = str(input("Direction: ")).upper()
+                if direction == 'E':
+                    return 3.3
+                elif direction == 'W':
+                    return 1.3
+                else:
+                    print('Not a valid direction!')
+
+def tile32(direction):
+    while direction != 'N' and direction != 'S':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'N':
+            return 3.3
+        elif direction == 'S':
+            return 3.1
+        else:
+            print('Not a valid direction!')
+
+def tile33(direction):
+    while direction != 'S' and direction != 'W':
+        direction = str(input("Direction: ")).upper()
+        if direction == 'S':
+            return 3.2
+        elif direction == 'W':
+            return 2.3
+        else:
+            print('Not a valid direction!')
+
+location = 1.1
+
+while location != 3.1:
+    if location == 1.1:
         print('You can travel: (N)orth.')
-    if location == 12:
-        print('You can travel: (N)orth or (E)ast or (S)outh.')
-    if location == 13:
-        print('You can travel: (E)ast or (S)outh.')
-    if location == 23:
-        print('You can travel: (E)ast or (W)est.')
-    if location == 33 or location == 22:
-        print('You can travel: (W)est or (S)outh.')
-    if location == 32:
-        print('You can travel: (N)orth or (S)outh.')
-    if location == 32:
-        print('Victory!')
+        direction = ''
+        location = tile11(direction)
 
-location = 11
-direction = input('direction: ' )
-while location != 31:
-    get_direction(location)
-    direction = input('Direction: ')
-    move()
+    if location == 1.2:
+        print('You can travel: (N)orth or (E)ast or (S)outh.')
+        direction = 'W'
+        location = tile12(direction)
+
+    if location == 1.3:
+        print('You can travel: (E)ast or (S)outh.')
+        direction = ''
+        location = tile13(direction)
+
+    if location == 2.1:
+        print('You can travel: (N)orth.')
+        direction = ''
+        location = tile21(direction)
+
+    if location == 2.2:
+        print('You can travel: (S)outh or (W)est.')
+        direction = ''
+        location = tile22(direction)
+
+    if location == 2.3:
+        print('You can travel: (E)ast or (W)est.')
+        direction = ''
+        location = tile23(direction)
+
+    if location == 3.2:
+        print('You can travel: (N)orth or (S)outh.')
+        direction = ''
+        location = tile32(direction)
+    
+    if location == 3.3:
+        print('You can travel: (S)outh or (W)est.')
+        direction = ''
+        location = tile33(direction)
+
+print('Victory!')
